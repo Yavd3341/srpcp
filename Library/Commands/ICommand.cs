a429b33980d79b0d4168f991e755d9006d65502d
Illia Yavdoshchuk <yavd3341@gmail.com>
2022-11-28 09:16:08 +0200

    Basic interfaces

---
+++ Library/Commands/ICommand.cs
@@ -0,0 +1,7 @@
+﻿using System.Net;
+
+namespace FG.SRPCProtocol.Commands {
+	public interface ICommand {
+		void Execute(IClient client, IPEndPoint endPoint);
+	}
+}
---
+++ Library/IClient.cs
@@ -0,0 +1,16 @@
+﻿using FG.SRPCProtocol.Commands;
+using System;
+using System.Net;
+
+namespace FG.SRPCProtocol {
+	public interface IClient {
+		int Port { get; }
+
+		void Execute(ICommand command, IPEndPoint endPoint);
+
+		void Start(int port);
+		void Shutdown();
+
+		void Send(byte[] data, IPEndPoint endPoint);
+	}
+}
